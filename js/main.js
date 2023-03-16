0ff7897e9c0a5ba3102a97fe7921402e89322232
Mattia Moneta <moneta.mattia@yahoo.com>
2023-03-16 14:23:38 +0100

    Ok JS

--- js/main.js
+++ js/main.js
@@ -3,7 +3,44 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      throwError : false,
+      textField : "",
+      list : [
+        {
+            text: 'sample',
+            done: true
+        },
+        {
+            text: 'sample1',
+            done: false
+        },
+        {
+            text: 'sample2',
+            done: false
+        }
+      ]
+    }
+  },
+  methods : {
+    addItem(){
+        if(this.textField.length >= 5){
+            this.throwError = false;
+            this.list.push({text: this.textField, done: false});
+        } else {
+            this.throwError = true;
+        }
+    },
+    removeItem(index){
+        this.list.splice(index, 1);
+    },
+    toggleStatus(index){
+
+        if(this.list[index].done == true){
+            this.list[index].done = false;
+        } else {
+            this.list[index].done = true;
+        }
+
     }
   }
 }).mount('#app')
